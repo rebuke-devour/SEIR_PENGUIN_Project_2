dda08dc82f7decc7a8f926d8b96a72bde51245c9
John Torres <rebuke.devour@gmail.com>
2021-11-01 08:06:59 -0400

    Update, Crud is not complete, cannot update

--- controllers/Passwords.js
+++ controllers/Passwords.js
@@ -1,148 +1,148 @@
-// const express = require('express')
-// const Passwords = require('./models/Passwords')
+const express = require('express')
+const Passwords = require('./models/Passwords')
 
-// /////////////////////////////////////////
-// // Create Route
-// /////////////////////////////////////////
-// const router = express.Router();
+/////////////////////////////////////////
+// Create Route
+/////////////////////////////////////////
+const router = express.Router();
 
-// ////////////////////////////////////////
-// // Router Middleware
-// ////////////////////////////////////////
-// // Authorization Middleware
-// router.use((req, res, next) => {
-//   if (req.session.loggedIn) {
-//     next();
-//   } else {
-//     res.redirect("/user/login");
-//   }
-// });
-// router.get('/seed', (req,res)=>{
+////////////////////////////////////////
+// Router Middleware
+////////////////////////////////////////
+// Authorization Middleware
+router.use((req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/user/login");
+  }
+});
+router.get('/seed', (req,res)=>{
 
-// const passwordSchema = new Schema({
-//     acctName: {type: String, required: true, default: false},
-//     userName: {type: String, Required: true, default: false},
-//     passwordKey: {type: String, required: true, default: false},
+const passwordSchema = new Schema({
+    acctName: {type: String, required: true, default: false},
+    userName: {type: String, Required: true, default: false},
+    passwordKey: {type: String, required: true, default: false},
    
-// })
+})
 
-// })
+})
 
 
-// // index route - get - /fruits
-// router.get("/", (req, res) => {
-//     //find all the fruits
-//     Fruit.find({username: req.session.username})
-//     .then((fruits) => {
-//         // render the index template with the fruits
-//         res.render("fruits/index.liquid", {fruits})
-//     })
-//     // error handling
-//     .catch((error) => {
-//         res.json({error})
-//     })
-// })
+// index route - get - /fruits
+router.get("/", (req, res) => {
+    //find all the fruits
+    Passwords.find({username: req.session.username})
+    .then((fruits) => {
+        // render the index template with the fruits
+        res.render("keymaster/index.liquid", {fruits})
+    })
+    // error handling
+    .catch((error) => {
+        res.json({error})
+    })
+})
 
-// // new route - get request - /fruits/new
-// router.get("/new", (req, res) => {
-//     res.render("fruits/new.liquid")
-// })
+// new route - get request - /fruits/new
+router.get("/new", (req, res) => {
+    res.render("keymaster/new.liquid")
+})
 
-// // create - post request - /fruits
-// router.post("/", (req, res) => {
+// create - post request - /fruits
+router.post("/", (req, res) => {
 
-//     // convert the checkbox property to true or false
-//     req.body.readyToEat = req.body.readyToEat === "on" ? true : false
+    // convert the checkbox property to true or false
+    req.body.saved = req.body.saved === "on" ? true : false
 
-//     // add the username to req.body, to track user
-//     req.body.username = req.session.username
+    // add the username to req.body, to track user
+    req.body.username = req.session.username
 
-//     // create the new fruit
-//     Passwords.create(req.body)
-//     .then((fruit) => {
-//         // redirect the user back to the index route
-//         res.redirect("/fruits")
-//     })
-//     // error handling
-//     .catch((error) => {
-//         res.json({error})
-//     })
+    // create the new fruit
+    Passwords.create(req.body)
+    .then((fruit) => {
+        // redirect the user back to the index route
+        res.redirect("/fruits")
+    })
+    // error handling
+    .catch((error) => {
+        res.json({error})
+    })
 
-// })
+})
 
-// // edit route - get request - /fruits/:id/edit
-// router.get("/:id/edit", (req, res) => {
-//     // get the id from params
-//     const id = req.params.id
+// edit route - get request - /fruits/:id/edit
+router.get("/:id/edit", (req, res) => {
+    // get the id from params
+    const id = req.params.id
 
-//     // get the fruit with the matching id
-//     Fruit.findById(id)
-//     .then((fruit) => {
-//         // render the edit page template with the fruit data
-//         res.render("fruits/edit.liquid", { fruit })
-//     })
-//     // error handling
-//     .catch((error) => {
-//         res.json({error})
-//     })
-// })
+    // get the fruit with the matching id
+    Passwords.findById(id)
+    .then((passwords) => {
+        // render the edit page template with the fruit data
+        res.render("fruits/edit.liquid", { passwords: keymaster })
+    })
+    // error handling
+    .catch((error) => {
+        res.json({error})
+    })
+})
 
-// // update route - put request - "/fruits/:id"
-// router.put("/:id", (req, res) => {
-//     // get the id from params
-//     const id = req.params.id
+// update route - put request - "/fruits/:id"
+router.put("/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id
     
-//     // convert the checkbox property to true or false
-//     req.body.readyToEat = req.body.readyToEat === "on" ? true : false
+    // convert the checkbox property to true or false
+    req.body.saved = req.body.readyToEat === "on" ? true : false
 
-//     // update the item with the matching id
-//     Fruit.findByIdAndUpdate(id, req.body, {new: true})
-//     .then((fruit) => {
-//         // redirect user back to index
-//         res.redirect("/fruits")
-//     })
-//      // error handling
-//      .catch((error) => {
-//         res.json({error})
-//     })
-// }
-// )
+    // update the item with the matching id
+    Passwords.findByIdAndUpdate(id, req.body, {new: true})
+    .then((fruit) => {
+        // redirect user back to index
+        res.redirect("/keymaster")
+    })
+     // error handling
+     .catch((error) => {
+        res.json({error})
+    })
+}
+)
 
-// // destroy route - delete request - /fruits/:id
-// router.delete("/:id", (req, res) => {
-//     // grab the id from params
-//     const id = req.params.id
-//     // delete the fruit
-//     Fruit.findByIdAndRemove(id)
-//     .then((fruit) => {
-//         // redirect user back to index
-//         res.redirect("/fruits")
-//     })
-//      // error handling
-//      .catch((error) => {
-//         res.json({error})
-//     })
-// })
+// destroy route - delete request - /fruits/:id
+router.delete("/:id", (req, res) => {
+    // grab the id from params
+    const id = req.params.id
+    // delete the fruit
+    Passwords.findByIdAndRemove(id)
+    .then((passwords) => {
+        // redirect user back to index
+        res.redirect("/keymaster")
+    })
+     // error handling
+     .catch((error) => {
+        res.json({error})
+    })
+})
 
-// // show route - get - /fruits/:id
-// router.get("/:id", (req, res) => {
-//     // get the id from params
-//     const id = req.params.id
+// show route - get - /fruits/:id
+router.get("/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id
 
-//     // get that particular fruit from the database
-//     Passwords.findById(id)
-//     .then((fruit) => {
-//         // render the show template with the fruit
-//         res.render("keymaster/show.liquid", {fruit})
-//     })
-//     // error handling
-//     .catch((error) => {
-//         res.json({error})
-//     })
-// })
+    // get that particular fruit from the database
+    Passwords.findById(id)
+    .then((passwords) => {
+        // render the show template with the fruit
+        res.render("keymaster/show.liquid", {paswords: keymaster})
+    })
+    // error handling
+    .catch((error) => {
+        res.json({error})
+    })
+})
 
 
-// /////////////////////////////
-// // export the router
-// /////////////////////////////
-// module.exports = router
+/////////////////////////////
+// export the router
+/////////////////////////////
+module.exports = router
